8eb9704b702b436cdec97e711047784b00746176
Ovi Sofariu <ovi@sofariu.com>
2025-04-26 13:13:22 +0000

    when I click the Disable node button I wish that node to be disabled. and the tree below to visually reflect that right away.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -135,6 +135,7 @@ export default function Home() {
         }
         return new StateTree(newStateTree.trees);
       });
+       setStateTree(prevState => new StateTree([...prevState.trees]));
     }
   };
 
@@ -148,6 +149,7 @@ export default function Home() {
         }
         return new StateTree(newStateTree.trees);
       });
+      setStateTree(prevState => new StateTree([...prevState.trees]));
     }
   };
 
@@ -263,4 +265,3 @@ export default function Home() {
     </SidebarProvider>
   );
 }
-
